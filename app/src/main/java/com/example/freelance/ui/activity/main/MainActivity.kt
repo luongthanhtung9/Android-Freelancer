33f3fbbe00ca6442df522c70f8378f39b58ee78a
Haich <daicavipken1@gmail.com>
2022-09-04 10:53:06 +0700

    demo code activity

--- app/src/main/java/com/example/freelance/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/example/freelance/ui/activity/main/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.freelance.ui.activity.main
 
+import androidx.viewbinding.ViewBinding
+import com.example.freelance.databinding.ActivityMainBinding
 import com.example.freelance.ui.base.BaseActivity
 import com.example.freelance.ui.base.BaseViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity: BaseActivity() {
-    override val viewModel: BaseViewModel
-        get() = TODO("Not yet implemented")
+    override val viewModel: MainViewModel by viewModel()
 
+    override val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun initListener() {
 
     }
